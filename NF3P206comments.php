4c0ad45e8bcae3ab50d421334c79e18b22b5c34d
daw2-gonzalez21 <71894613+daw2-gonzalez21@users.noreply.github.com>
2020-10-26 20:14:04 +0100

    Add files via upload

---
+++ NF3P206comments.php
@@ -0,0 +1,25 @@
+<html>
+ <head>
+  <title>Comentarios</title>
+ </head>
+ <body>
+     <h2>Lista de 10 conceptos importantes:</h2>
+     <ul>
+         <li>$query</li>
+         <li>SELECTS</li>
+         <li>LIMIT</li>
+         <li>FOR</li>
+         <li>registros</li>
+         <li>Codenvy</li>
+         <li>INSERTS</li>
+         <li>PHP</li>
+         <li>PUSH TO GITHUB</li>
+         <li>MYSQL</li>
+     </ul>
+     <p>Califica el documento: 7</p>
+     <p>Califica la explicación del profesor: 8</p>
+     <p>Me ha costado un poco realizar todo pero lo he podido solventar (creo).
+        No sé que haria sin los videos de Youtube.
+     </p>
+ </body>
+</html>
